4333af379ea43791d9ac2fe37bba863b1b89853f
TanishqSrivastava <yashu_comp@hotmail.com>
2018-12-05 14:58:31 +0530

    blah blah

---
+++ 2.5DPlatformerGame/Assets/Scripts/Follow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour {
+    public Transform tran;
+    public Vector3 coordsOfCam;
+    public float tranPosY;
+    public Vector3 newPosition;
+    // Use this for initialization
+	void Start () {
+		
+	}
+    private void Update()
+    {
+        
+    }
+    // Update is called once per frame
+    void LateUpdate () {
+        newPosition.x = tran.position.x;
+        newPosition.y = transform.position.y;
+        newPosition.z = transform.position.z;
+        transform.position = newPosition;
+	}
+}
