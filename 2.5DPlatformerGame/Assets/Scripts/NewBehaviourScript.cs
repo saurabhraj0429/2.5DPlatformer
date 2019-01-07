17630b7a2d024959e78cff0c968d5674c3b4d7f9
TanishqSrivastava <yashu_comp@hotmail.com>
2019-01-07 23:54:26 +0530

    blah blah blah
    zdbzgsgsreg

--- 2.5DPlatformerGame/Assets/Scripts/NewBehaviourScript.cs
+++ 2.5DPlatformerGame/Assets/Scripts/NewBehaviourScript.cs
@@ -10,12 +10,16 @@ public class NewBehaviourScript : MonoBehaviour {
     public float fallMultiplier = 2;
     public float jumpSpeed;
     public bool isGrounded;
+    public float xVel;
+    public float gravity;
+    public float speed;
     // Use this for initialization
 	void Start () {
-        
+       
         tran = GetComponent<Transform>();
         rb = GetComponent<Rigidbody>();
-	}
+        speed = 10;
+    }
 
     // Update is called once per frame
     private void OnCollisionEnter(Collision collision)
@@ -25,15 +29,19 @@ public class NewBehaviourScript : MonoBehaviour {
 
         }
     }
-    void Update () {
-        if (isGrounded == false && Input.GetKeyDown("w")) {
-            
+    void Update()
+    {
+        Physics.gravity = new Vector3(0, gravity, 0);
+        if (isGrounded == false && Input.GetKeyDown("w"))
+        {
+
 
         }
-        if (Input.GetKeyDown("w") && isGrounded == true) {
+        if (Input.GetKeyDown("w") && isGrounded == true)
+        {
             isGrounded = false;
             rb.velocity = Vector3.up * jumpSpeed;
-            
+
 
         }
         if (rb.velocity.y < 0)
@@ -42,8 +50,37 @@ public class NewBehaviourScript : MonoBehaviour {
 
 
         }
-        moveInputX = Input.GetAxis("Horizontal");
-        tran.Translate(moveInputX * 10 * Time.deltaTime, 0, 0);
         
-	}
+       moveInputX = Input.GetAxisRaw("Horizontal");
+       tran.Translate(moveInputX * speed * Time.deltaTime, 0, 0);
+        if (Input.GetKeyDown("d")) {
+            Invoke("SpeedMovement" , 0.2f);
+
+        }
+        if (Input.GetKeyUp("d")) {
+            speed = 10;
+
+
+        }
+        if (Input.GetKeyDown("a"))
+        {
+            Invoke("SpeedMovement", 0.2f);
+
+        }
+        if (Input.GetKeyUp("a"))
+        {
+            speed = 10;
+
+
+        }
+
+
+
+
+    }
+    void SpeedMovement() {
+        speed = 15;
+
+
+    }
 }
